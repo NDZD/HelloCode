19a35a5d36ad7c73fc4a13668f6cb63bec2c727b
ndzd <n_level@mail.ru>
2022-08-22 23:54:10 +0300

    if else

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
